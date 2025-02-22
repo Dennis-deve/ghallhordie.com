2a1494b16d869e1f606ac0e9d044bb4efa1a2833
Dennis Samuel <dsasante-asare@st.ug.edu.gh>
2025-02-22 13:43:15 +0000

    Transformed the site from 6-months anniversary to birthday celebration

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-
 // Display the current year in the footer
 document.getElementById("year").textContent = new Date().getFullYear();
 
-// Function to fade in the encouragement section
+// Function to fade in the message section
 document.addEventListener('DOMContentLoaded', function () {
-  const encouragementSection = document.querySelector('.encouragement-section');
-  encouragementSection.style.opacity = 0;
-  encouragementSection.style.transition = 'opacity 2s ease-in-out';
+  const messageSection = document.querySelector('.message-section');
+  messageSection.style.opacity = 0;
+  messageSection.style.transition = 'opacity 2s ease-in-out';
 
-  // Delay the appearance by 1 second for a smooth effect
   setTimeout(() => {
-    encouragementSection.style.opacity = 1;
+    messageSection.style.opacity = 1;
   }, 1000);
+});
+
+// Function to play multiple videos in the hero section
+document.addEventListener('DOMContentLoaded', function () {
+  const videos = [
+    'images/V1.mp4',
+    'images/V2.mp4',
+    'images/V3.mp4',
+   
+  ];
+  
+  let currentVideoIndex = 0;
+  const heroVideo = document.querySelector('.hero-video');
+  
+  heroVideo.src = videos[currentVideoIndex];
+  heroVideo.play();
+  
+  heroVideo.addEventListener('ended', function () {
+    currentVideoIndex = (currentVideoIndex + 1) % videos.length;
+    heroVideo.src = videos[currentVideoIndex];
+    heroVideo.play();
+  });
 });
